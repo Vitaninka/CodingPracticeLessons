5c27075d109aa3deb5eefdbeb1c816b59cda9b6d
Irina Behrend <irinabnd@gmail.com>
2021-05-09 08:27:12 -0500

    Forking

--- src/main/java/AscendingSequence.java
+++ src/main/java/AscendingSequence.java
@@ -1,11 +1,14 @@
 public class AscendingSequence {
 
-    public int[] buildAscendingArray (int endNumber) {
-        int[] array = new int[endNumber];
+    public int[] buildAscendingArray (int startNumber, int endNumber) {
 
-        for (int i = 1; i <= endNumber; i ++) {
+        int[] array = new int[endNumber - startNumber + 1];
+
+        for (int i = 1; i < endNumber - startNumber + 1; i ++) {
             array[i - 1] = i;
+            System.out.println(array[i]);
         }
         return array;
+
     }
 }
--- src/test/java/AscendingSequenceTest.java
+++ src/test/java/AscendingSequenceTest.java
@@ -5,12 +5,67 @@ public class AscendingSequenceTest {
 
     @Test
     public void testAscendingArrayHappyPath () {
+
+        //A1
+        int startNumber = 1;
         int endNumber = 5;
         int[] expectedResult = {1, 2, 3, 4, 5};
 
+        //A2
         AscendingSequence ascendingSequence = new AscendingSequence();
-        int[] actualResult = ascendingSequence.buildAscendingArray(endNumber);
+        int[] actualResult = ascendingSequence.buildAscendingArray(startNumber, endNumber);
 
+        //A3
         Assert.assertArrayEquals(expectedResult, actualResult);
+
     }
+
+        @Test
+        public void testAscendingZeroCase() {
+
+        //A1
+        int startNumber = 0;
+        int endNumber = 5;
+        int[] expectedResult = {0,1, 2, 3, 4, 5};
+
+        //A2
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.buildAscendingArray(startNumber, endNumber);
+
+        //A3
+        Assert.assertArrayEquals(expectedResult, actualResult);
+        }
+
+        @Test
+        public void testAscendingNegativeNumber(){
+
+        //A1
+        int startNumber = -8;
+        int endNumber = -1;
+        int[] expectedResult = {-1,-2,-3,-4,-5,-6,-7,-8};
+
+        //A2
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.buildAscendingArray(startNumber, endNumber);
+
+        //A3
+        Assert.assertArrayEquals(expectedResult, actualResult);
+
+        }
+
+        @Test
+        public void testAscendingNegativePositiveNumber(){
+
+        //A1
+        int startNumber = -4;
+        int endNumber = 5;
+        int[] expectedResult = {1, 2, 3, 4, 5};
+
+        //A2
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.buildAscendingArray(startNumber, endNumber);
+
+        //A3
+        Assert.assertArrayEquals(expectedResult, actualResult);
+        }
 }
